523d80de9e982be97b4084ae36edef6f0bd2e7a5
Molo Xiao <molo.xiao@gmail.com>
2015-01-27 15:04:42 +0800

    增加结算标题
    增加结算标题
    去掉不需要的内容

--- Classes/GameResultLayer.cpp
+++ Classes/GameResultLayer.cpp
@@ -8,9 +8,14 @@ bool GameResultLayer::init(){
 	}
 	Audio::getInstance()->playBGM("Music/bg.mp3");
 	Size visibleSize = Director::getInstance()->getVisibleSize();
+	
 	Sprite* background = Sprite::create("pr_bg.png");
 	background->setPosition(visibleSize.width/2,visibleSize.height/2);
 	this->addChild(background,-1);
+
+	Sprite* title = Sprite::create("co_win.png");
+	title->setPosition(visibleSize.width/2,visibleSize.height - 100);
+	this->addChild(title, 0);
 
 	return true;
 }

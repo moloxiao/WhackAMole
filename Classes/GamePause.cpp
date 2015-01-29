bb33fbc2e8253c3766425007d6921a5bc5f4b0b7
Molo Xiao <molo.xiao@gmail.com>
2015-01-29 13:28:52 +0800

    暂停界面添加打洞示例

--- Classes/GamePause.cpp
+++ Classes/GamePause.cpp
@@ -8,11 +8,25 @@ bool GamePause::init(){
 
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 
+	//========================================
+	ClippingNode* clippingNode = ClippingNode::create();
+	Node* stencil = Node::create(); 
+
+	auto Mole3X1 = Sprite::create("mouse_hole.png");
+	Mole3X1->setPosition(90, 100);
+	stencil->addChild(Mole3X1);
+	clippingNode->setStencil(stencil);
+
 	auto draw = DrawNode::create();
 	draw->drawSolidRect(Vec2(0,0), Vec2(480,800), Color4F::BLACK);
-	this->addChild(draw);
+	clippingNode->addChild(draw); 
 
-	// TODO : 等待添加挖洞效果
+	clippingNode->setInverted(true);     
+	clippingNode->setAlphaThreshold(0.5f);
+
+	this->addChild(clippingNode);
+
+	//========================================
 	
 
 	auto btnPause = MenuItemImage::create(
